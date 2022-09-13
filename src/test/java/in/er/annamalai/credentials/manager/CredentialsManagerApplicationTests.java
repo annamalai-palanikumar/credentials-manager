f6ca5128c7701b7492f8537b5d78e6c4b9b491cd
Annamalai Palanikumar <annamalai.p@outlook.in>
2022-09-14 01:18:22 +0200

    Intial application setup.

---
+++ src/test/java/in/er/annamalai/credentials/manager/CredentialsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.er.annamalai.credentials.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
